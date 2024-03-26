4ea367d8bc99503dc64263d436769a1c395be47a
Manami Hayashi <manami.hayashi@student.kdg.be>
2024-03-26 14:03:59 +0100

    UserDim at least worked with test-db and changed datbase_OP in config.py

--- data warehouse/dim/config.py
+++ data warehouse/dim/config.py
@@ -4,7 +4,7 @@ SERVER = 'localhost'
 DSN = 'sqlserver'
 USERNAME = 'SA'
 PASSWORD = 'DB3&3DB&DB3'
-DATABASE_OP = 'catchem'
+DATABASE_OP = 'catchem_9_2023'
 DATABASE_DWH = 'catchem_dwh'
 DRIVER = '{ODBC Driver 17 for SQL Server}'
 
